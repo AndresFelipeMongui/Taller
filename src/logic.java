f13f495abbb517d17c21fa03b029f4b804ca32da
AndresFelipeMongui <andres.mongui@uptc.edu.co>
2023-11-05 21:15:17 -0500

    proyecto

---
+++ src/logic.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import logic.work;
+public class logic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner imput=new Scanner(System.in);
+		   String menu = "<<<<<< MENU PRINCIPAL >>>>>\n\n" +
+	                "1.Numeros Romanos\n" +
+	                "2. Egolatras\n" +
+	                "3.Opcion 3\n\n" +
+	                "X. Salir";
+		   char option = 0;
+		   do{
+	            System.out.println( menu );
+
+				option = imput.nextLine().charAt(0);
+
+	            switch(option){
+	                case '1' :;
+	                break;
+
+	                case '2' : egolatr();
+	                break;
+
+	                case '3' : chains();
+	            }
+
+	        }while (Character.toUpperCase(option) != 'X');
+	}
+
+	private static void chains() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void egolatr() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
---
+++ src/logic/work.java
@@ -0,0 +1,29 @@
+package logic;
+
+public class work {
+	
+	private int x;
+	  public static String convertToRoman(int number){
+
+	        return null;
+	    }
+
+	    public static String calcFactors(int number){
+
+	        return null;
+	    }
+
+	    /**
+	     *
+	     * @param number
+	     * @return
+	     */
+	    static public boolean isEgolatr( int number){
+
+	        return false;
+	    }
+
+
+	}
+
+
