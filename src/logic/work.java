d5e13f9310196b99c375f75676c38a02f0f5f67e
Camilo9237 <150066907+Camilo9237@users.noreply.github.com>
2023-11-10 21:29:51 -0500

    Update work.java

--- src/logic/work.java
+++ src/logic/work.java
@@ -1,12 +1,25 @@
 package logic;
 
 public class work {
-	
-	
-	  public static String convertToRoman(int number){
+	//Convertir un número ingresado por el usuario a número Romano
+	public static String converttoRoman(int number){
+        String [] units = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        String [] tens = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        String [] hundreds = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        String [] thousands = {"","M","MM","MMM"};
+        int thousand, hundred, ten, unit;
 
-	        return null;
-	    }
+        thousand = number / 1000;
+        number = number % 1000;
+        hundred = number / 100;
+        number = number % 100;
+        ten = number / 10;
+        number = number % 10;
+        unit = number;
+
+        return thousands[thousand] + hundreds[hundred] + tens[ten] + units[unit];
+    }
+	  
 
 	    public static String calcFactors(int number){
 
