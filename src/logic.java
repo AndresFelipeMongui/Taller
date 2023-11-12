4d800d3fab74ed981b5dc033950534c4ada330ad
AndresFelipeMongui <andres.mongui@uptc.edu.co>
2023-11-11 19:44:47 -0500

    se agregan los puntos 3 y 4

--- src/logic.java
+++ src/logic.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
+import java.lang.Math;
 import logic.work;
 public class logic {
-	private Scanner input;
 	
 	 public logic() {
-	        input = new Scanner(System.in);
+	       Scanner input = new Scanner(System.in);
 	    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		Scanner input=new Scanner(System.in);
-		System.out.println("este es un mensaje nuevo");
+
 		   String menu = "<<<<<< MENU PRINCIPAL >>>>>\n\n" +
 	                "1.Numeros Romanos\n" +
-	                "2. Factores Primos\n" +
+	                "2.Factores Primos\n" +
 	                "3.Nombre Propio\n" +
 	                "4.Números Ególatras\n"+
 	                "5.Número Mágico\n" +
@@ -26,14 +26,43 @@ public class logic {
 				option = input.nextLine().charAt(0);
 
 	            switch(option){
-	                case '1' : Roman();
+	                case '1' : ;
 	                break;
 
 	                case '2' :;
 	                break;
-	                case '3' : ;
+	                case '3' :work.name(" ") ;
+	                Scanner Scan=new Scanner(System.in);
+	        		System.out.println("Ingrese el texto");
+	        		// se ingresa la cadena y se eliminan los espacios de inicio y final
+	        		String cadena=Scan.nextLine().trim();
+	        		String name=work.name(cadena);
+	        		System.out.println(name);
+	        		
+	        		
+	        		
+	        		
 	                break;
-	                case '4':;
+	                case '4':work.isEgolat(0);
+	                Scanner scan = new Scanner(System.in);
+	                int number=0;
+	                boolean letter=false;
+	                do	{
+	                	// se crea la excepcion cuando el usuario digite una letra       
+	                try	{
+	       		 System.out.print("Digite el numero: ");
+	       		 
+	       		  number = Integer.parseInt(scan.nextLine());
+	       		  letter=true;
+	                }catch(java.lang.NumberFormatException excepcion)	
+	                {System.out.println("Ingrese un numero");	}
+	                }while(letter==false);// solo avanza si se digita un numero
+	       		boolean isEgolatra = work.isEgolat(number);
+	       		
+			       if(isEgolatra)	{
+			    	   System.out.println("El numero ingresado es un numero egolatra");
+			       }else	{System.out.println("El numero ingresado no es un numero egolatra");}
+	       		 
 	                break;
 	                case '5':;
 	                break;
@@ -42,15 +71,7 @@ public class logic {
 
 	        }while (Character.toUpperCase(option) != 'X');
 	}
-
-	private static void Roman() {
-		// TODO Auto-generated method stub
-		Scanner input=new Scanner(System.in);
-		System.out.println("Ingrese el numero a convertir:");
-		int number=input.nextInt();
-		
-	}
-
+	
 	
 
 }
--- src/logic/work.java
+++ src/logic/work.java
@@ -2,28 +2,54 @@ package logic;
 
 public class work {
 	
-	
-	  public static String convertToRoman(int number){
-
-	        return null;
+	   
+	    public static String name(String cadena)	{
+	    String [] palabras=cadena.split(" ");// se va a dividir cada que encuentre un espacio vacio
+	    	String resultado = null;
+	    	StringBuilder result=new StringBuilder(); 
+	    for(int i=0;i<palabras.length;i++)	{
+			//se obtiene la primer letra de cada palabra
+			String palabra=palabras[i];
+			try	{
+			String iniciales=palabra.substring(0,1); String second=palabra.substring(1);
+			String Mayor = iniciales.toUpperCase();
+			String end=Mayor + second;
+			result.append(end).append(" ");
+			}catch( java.lang.StringIndexOutOfBoundsException excepcion) {// se gestiona cuando existan mas de dos espacios 
+				
+			}
+		}
+		
+		System.out.println();
+			return result.toString();
+	    	
 	    }
+	    
+	  //se define el metodo para convertir y comparar el numero egolatra
+	     public static boolean isEgolat( int number){
+	    	//convierte el numero en una cadena
+			 String numberString = Integer.toString(number);
+			// vector donde se guardan cada uno de los digitos
+			int [] vector= new int[numberString.length()]; 
+			//un for para llenar el vector con los digitos correspondientes
+			 for (int i = 0; i < numberString.length(); i++) {
+		            char digito =numberString.charAt(i);
+		    
+		          //convierte el caracter en un int
+		      vector[i]=Character.getNumericValue(digito); 
+		    }
+		            
+			         
+			  int suma=0;
+		      for (int i= 0; i < numberString.length(); i++)	{
+		      int  resultado=(int)Math.pow(vector[i],numberString.length());// eleva cada uno de los digitos por la longitud
+		      suma=suma+resultado;
+		            }
+		      return number == suma;	
+		      
+		      
+}	       
+}	
 
-	    public static String calcFactors(int number){
-
-	        return null;
-	    }
-
-	    /**
-	     *
-	     * @param number
-	     * @return
-	     */
-	    static public boolean isEgolatr( int number){
-
-	        return false;
-	    }
-
-
-	}
 
 
